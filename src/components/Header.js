247519056870894fabee56c0609bd3247bfcb849
Akisa-Moruri <akisamoruri@gmail.com>
2024-04-26 07:50:25 +0300

    mini project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Header = ({ name }) => {
+function Header({ name }) {
   return (
-  <header>
-    <h1>{name}</h1>
+    <header>
+      <h1>{name}</h1>
     </header>
   );
-};
+}
 
 export default Header;
